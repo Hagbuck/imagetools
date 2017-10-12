2e373aa478a79bbd0bb4682c05616916823dd413
Hackbug <anthony.vuillemin@outlook.fr>
2017-10-12 14:29:40 +0200

    main.c added

---
+++ src/main.c
@@ -0,0 +1,14 @@
+/**
+ * [POLYTECH APP3] Projet base de developpement en C
+ * Detcheberry Valentin
+ * Vuillemin Anthony
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	printf("Hello World!\n");
+	return 0;
+}
